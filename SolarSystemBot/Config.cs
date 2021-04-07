dd881332cc97c222ae843ea1034cf77946f96c9e
Nevermind <1276411@bsu.edu.ru>
2021-04-07 23:13:32 +0300

    о

--- SolarSystemBot/Config.cs
+++ SolarSystemBot/Config.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+
 namespace SolarSystemBot
 {
-    public class Config
+    public static class Config
     {
-        public Config()
-        {
-        }
+        public static string Token { get; set; } = "1702184934:AAE0NsCeI7vd3aYAPwgsRbI4xuaKkmwKZ8U";
+
     }
 }
