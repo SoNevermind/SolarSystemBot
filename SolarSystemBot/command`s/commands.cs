6ef9b16c12457b771fbdf93fd09efc1c73b02bdd
Stast <60179402+Stasyan4444@users.noreply.github.com>
2021-04-07 23:50:13 +0300

    Contains
    Contains

---
+++ SolarSystemBot/command`s/commands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+
+namespace SolarSystemBot.command_s
+{
+    public abstract class commands
+    {
+        public abstract string[] Names { get; set; }
+
+        public abstract void Execute(Message messege, TelegramBotClient client);
+
+        public bool Contains(string messege)
+        {
+            foreach (var mess in Names)
+            {
+                if (messege.Contains(mess))
+                {
+                    return true;
+                }
+
+            }return false;
+        }
+
+
+    }
+}
