a6d1df2bebb0b690bb6d0bf71bafbec1955eb6a1
Nevermind <1276411@bsu.edu.ru>
2021-04-08 00:03:10 +0300

    Получение ID

---
+++ SolarSystemBot/command`s/GetMyIdCommands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace SolarSystemBot.command_s
+{
+    public class GetMyIdCommands : commands
+    {
+        public override string[] Names { get; set; } = { "getmyid", "get_my_id", "мой айди" };
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Ваш телеграм ID: " + message.From.Id);
+        }
+    }
+}
